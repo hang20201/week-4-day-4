51746403a866115d3ca0018aba6cc614e7edb156
Sean Collier <sean.collier928@yahoo.com>
2021-04-01 13:35:29 -0700

    working

--- local-storage-project/storage-project.js
+++ local-storage-project/storage-project.js
@@ -1,15 +1,32 @@
 window.addEventListener("DOMContentLoaded", (event) => {
 
-  // const showCart = () => {
+  const showCart = () => {
+    const shopCart = document.getElementById("shopping-cart")
+    document.addEventListener('click', (event) => {
+      // let item = document.getElementById('items');
+      for(let i = 0; i < localStorage.length; i++) {
+        let element = localStorage.key(i);
+         let elementContainer = document.createElement("div");
+         elementContainer.setAttribute("id", element);
+        //  localStorage.push()
+        // localStorage.getItem(element[i]);
+        shopCart.appendChild(elementContainer);
+      }
+    })
+  };
 
-  // };
+  const storeItem = () => {
+    const addItem = document.getElementById("add-to-cart")
+    addItem.addEventListener('click', (event) => {
+      let item = document.getElementById('items');
+      let value =  item.value;
+      localStorage.setItem(item, value);
+    })
+  };
 
-  // const storeItem = () => {
+  const removeItem = () => {
 
-  // };
-
-  // const removeItem = () => {
-
-  // };
-
+  };
+storeItem()
+showCart()
 });
